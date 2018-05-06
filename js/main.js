bb45333c39a549505161a3a0b656bae750139406
TgorHub <jkkortelahti@gmail.com>
2018-05-06 16:22:16 +0300

    Add more descriptive alt text for restaurant images

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = 'Restaurant image';
+  image.alt = 'Image of restaurant ' + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
